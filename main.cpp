d1f001fca7fdd82e7594610454d44b57783c28a9
fferrlop <fferrlop@myuax.com>
2023-12-14 12:09:01 +0100

    IMprim en C

--- main.cpp
+++ main.cpp
@@ -3,22 +3,29 @@
 #include <stdio.h>
 #include <string.h>
 
+//struct C++
 struct estudiante {
     std::string nombre;
     int edad;
     float promedio;
 };
 
+//struct C
 struct estudianteC {
     char nombre[30];
     int edad;
     float promedio;
 };
 
+//imprimir C++
 void imprimirEstudiante(estudiante est) {
     std::cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << std::endl;
 }
 
+//imprimir C
+void imprimirEstudianteC(estudianteC est) {
+    std::cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << std::endl;
+}
 
 int main() {
 
