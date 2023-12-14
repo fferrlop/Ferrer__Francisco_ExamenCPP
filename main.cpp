dd3a7fbfaf94fcab4575af7e4334b89199d9b7f1
fferrlop <fferrlop@myuax.com>
2023-12-14 11:56:35 +0100

    struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <cstring>
 
+struct estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+};
+
+void imprimirEstudiante(estudiante est) {
+
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
