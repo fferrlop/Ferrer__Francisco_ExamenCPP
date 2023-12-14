b30655a3e06b5922f9e6af3d508c6f77d7500975
fferrlop <fferrlop@myuax.com>
2023-12-14 12:00:05 +0100

    1.mainC++

--- main.cpp
+++ main.cpp
@@ -10,7 +10,20 @@ struct estudiante {
 void imprimirEstudiante(estudiante est) {
     std::cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << std::endl;
 }
+
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    estudiante Estudiante1;
+
+            Estudiante1.nombre = "Fran";
+            Estudiante1.edad = 20;
+            Estudiante1.promedio = 7.5;
+
+
+    imprimirEstudiante(Estudiante1);
+
     return 0;
 }
